7e6473d1d82dbbb4fe9c4d018d835d26853b81b7
h-programming12 <h.programming12@gmail.com>
2023-12-09 22:10:29 +0900

    Kakao Map으로 커스텀 마커 구현하기

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -40,12 +40,16 @@ export default function Map() {
       rooms?.map((room) => {
         // 마커가 표시될 위치입니다
         const markerPosition = new window.kakao.maps.LatLng(room.lat, room.lng)
-        // 마커를 생성합니다
-        const marker = new window.kakao.maps.Marker({
+        // custom overlay를 설정해줍니다
+        const content = `<div class="custom_overlay">${room.price?.toLocaleString()}원</div>`
+
+        // custom overlay를 생성합니다
+        const customOverlay = new window.kakao.maps.CustomOverlay({
           position: markerPosition,
+          content: content,
         })
-        // 마커가 지도 위에 표시되도록 설정합니다
-        marker.setMap(map)
+        // 커스텀 오버레이가 지도 위에 표시되도록 설정합니다
+        customOverlay.setMap(map)
       })
     })
   }
