4aff052fe1d70fa7a423d4ea0684c959a3117c74
h-programming12 <h.programming12@gmail.com>
2024-01-17 23:26:34 +0900

    Loading UI로 스켈레톤 적용하기

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -33,3 +33,15 @@ export function FullPageLoader() {
     </div>
   )
 }
+
+export function PrimaryLoader({ className }: { className?: string }) {
+  return (
+    <div className="min-h-screen flex flex-col justify-center z-50">
+      <div className="flex gap-5 items-center justify-center">
+        <div className="w-2 h-2 rounded-full bg-rose-600 animate-ping" />
+        <div className="w-2 h-2 rounded-full bg-rose-600 animate-ping" />
+        <div className="w-2 h-2 rounded-full bg-rose-600 animate-ping" />
+      </div>
+    </div>
+  )
+}
